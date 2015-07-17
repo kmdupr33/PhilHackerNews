ed89fe6806a7e82a75c851a296ec6a8d13e54810
MattDupree <kmdupr33@gmail.com>
2015-07-17 06:50:48 -0400

    removed unnecessary log statement

--- app/src/androidTest/java/com/philosophicalhacker/philhackernews/DaggerModuleOverridingAndroidJUnitRunner.java
+++ app/src/androidTest/java/com/philosophicalhacker/philhackernews/DaggerModuleOverridingAndroidJUnitRunner.java
@@ -13,7 +13,6 @@ public class DaggerModuleOverridingAndroidJUnitRunner extends AndroidJUnitRunner
     @Override
     public Application newApplication(ClassLoader cl, String className, Context context) throws InstantiationException, IllegalAccessException, ClassNotFoundException {
         String canonicalName = TestApplication.class.getCanonicalName();
-        Log.d("class name", canonicalName);
         return super.newApplication(cl, canonicalName, context);
     }
 }
