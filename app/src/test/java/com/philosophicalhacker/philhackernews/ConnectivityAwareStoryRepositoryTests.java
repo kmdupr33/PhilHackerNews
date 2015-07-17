264003527889df42fac7edc4b8f17144aeda5a41
MattDupree <kmdupr33@gmail.com>
2015-07-17 17:26:47 -0400

    got ConnectivityAwareStoryRepository tests to pass

--- app/src/test/java/com/philosophicalhacker/philhackernews/ConnectivityAwareStoryRepositoryTests.java
+++ app/src/test/java/com/philosophicalhacker/philhackernews/ConnectivityAwareStoryRepositoryTests.java
@@ -7,14 +7,19 @@ import com.philosophicalhacker.philhackernews.data.ConnectivityAwareStoryReposit
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.internal.verification.VerificationModeFactory;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.List;
 
+import rx.Subscription;
+import rx.functions.Action1;
 import rx.observables.ConnectableObservable;
 
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
 
 /**
  * Created by MattDupree on 7/17/15.
@@ -41,6 +46,6 @@ public class ConnectivityAwareStoryRepositoryTests {
         connectivityAwareStoryRepository.loadTopStories();
 
         //Assert
-        verify(mCachedStoriesObservable).connect();
+        verify(mCachedStoriesObservable).connect(any(Action1.class));
     }
 }
