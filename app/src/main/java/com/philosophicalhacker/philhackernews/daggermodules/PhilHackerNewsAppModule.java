0d967e08b990f828bca919b240d0b98649d32d9e
MattDupree <kmdupr33@gmail.com>
2015-07-19 10:27:13 -0400

    extracted DataModule from PhilHackerNewsAppModule

--- app/src/main/java/com/philosophicalhacker/philhackernews/daggermodules/PhilHackerNewsAppModule.java
+++ app/src/main/java/com/philosophicalhacker/philhackernews/daggermodules/PhilHackerNewsAppModule.java
@@ -1,30 +1,9 @@
 package com.philosophicalhacker.philhackernews.daggermodules;
 
-import android.content.ContentResolver;
 import android.content.Context;
-import android.database.Cursor;
-import android.net.ConnectivityManager;
-import android.support.v4.content.CursorLoader;
-
-import com.philosophicalhacker.philhackernews.data.CursorToStoryConverter;
-import com.philosophicalhacker.philhackernews.data.DataFetcher;
-import com.philosophicalhacker.philhackernews.data.content.CachedDataFetcher;
-import com.philosophicalhacker.philhackernews.data.remote.HackerNewsRestAdapter;
-import com.philosophicalhacker.philhackernews.data.DataConverter;
-import com.philosophicalhacker.philhackernews.data.content.HackerNewsContentProvider;
-import com.philosophicalhacker.philhackernews.data.content.HackerNewsData;
-import com.philosophicalhacker.philhackernews.data.content.HackerNewsDatabaseOpenHelper;
-import com.philosophicalhacker.philhackernews.data.remote.RemoteDataFetcher;
-import com.philosophicalhacker.philhackernews.data.sync.HackerNewsSyncService;
-import com.philosophicalhacker.philhackernews.model.Story;
-
-import java.util.List;
-
-import javax.inject.Named;
 
 import dagger.Module;
 import dagger.Provides;
-import retrofit.RestAdapter;
 
 /**
  * Provides top level dependencies for all objects in the app.
@@ -32,10 +11,7 @@ import retrofit.RestAdapter;
  * Created by MattDupree on 7/16/15.
  */
 @Module(library = true,
-        injects = {
-                HackerNewsContentProvider.class,
-                HackerNewsSyncService.class
-            }
+        includes = DataModule.class
         )
 public class PhilHackerNewsAppModule {
     private Context mApplicationContext;
@@ -47,50 +23,5 @@ public class PhilHackerNewsAppModule {
     @Provides
     Context provideContext() {
         return mApplicationContext;
-    }
-
-    @Provides
-    ConnectivityManager provideConnectivityManager(Context context) {
-        return (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-    }
-
-    @Provides
-    ContentResolver provideContentResolver(Context context) {
-        return context.getContentResolver();
-    }
-
-    @Provides
-    HackerNewsRestAdapter privideHackerNewsDataSource() {
-        RestAdapter build = new RestAdapter.Builder()
-                .setEndpoint("https://hacker-news.firebaseio.com/v0")
-                .setLogLevel(RestAdapter.LogLevel.FULL)
-                .build();
-        return build.create(HackerNewsRestAdapter.class);
-    }
-
-    @Provides @Named(RemoteDataFetcher.DAGGER_INJECT_QUALIFIER)
-    DataFetcher provideRemoteDataFetcher(HackerNewsRestAdapter hackerNewsRestAdapter) {
-        return new RemoteDataFetcher(hackerNewsRestAdapter);
-    }
-
-    @Provides @Named(CachedDataFetcher.DAGGER_INJECT_QUALIFIER)
-    DataFetcher provideCachedDataFetcher(ContentResolver contentResolver, DataConverter<List<Story>, Cursor> dataConverter) {
-        return new CachedDataFetcher(contentResolver, dataConverter);
-    }
-
-    @Provides
-    CursorLoader provideStoryLoader(Context context) {
-        String sortOrder = HackerNewsData.Stories.SCORE + " DESC";
-        return new CursorLoader(context, HackerNewsData.Stories.CONTENT_URI, null, null, null, sortOrder);
-    }
-
-    @Provides
-    DataConverter<List<Story>, Cursor> provideCursorToStoryIdsLoaderDataConverter() {
-        return new CursorToStoryConverter();
-    }
-
-    @Provides
-    HackerNewsDatabaseOpenHelper provideHackerNewsDatabaseOpenHelper(Context context) {
-        return new HackerNewsDatabaseOpenHelper(context, "hackernewsdata.db", null, 1);
     }
 }
