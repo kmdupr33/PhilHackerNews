6e035745839455dadb93efd47588b2db0590579f
MattDupree <kmdupr33@gmail.com>
2015-07-21 11:35:14 -0400

    current fragment state is now kept upon orientation change

--- app/src/main/java/com/philosophicalhacker/philhackernews/ui/storydetail/StoryDetailActivity.java
+++ app/src/main/java/com/philosophicalhacker/philhackernews/ui/storydetail/StoryDetailActivity.java
@@ -16,9 +16,10 @@ public class StoryDetailActivity extends AppCompatActivity {
     private static final String EXTRA_STORY = "com.philosophicalhacker.philhackernews.EXTRA_STORY";
     private static final String STORY_FRAG_TAG = "story";
     private static final String COMMENT_FRAG_TAG = "comments";
+    private static final String SAVE_STATE_DETAIL_FRAG_SHOW = "SAVE_STATE_DETAIL_FRAG_SHOW";
     private StoryDetailFragment mStoryDetailFragment;
     private Fragment mCommentsFragment;
-    private Item mItem;
+    private Item mStoryItem;
 
     public static Intent getStartIntent(Context context, Item item) {
         Intent intent = new Intent(context, StoryDetailActivity.class);
@@ -30,34 +31,65 @@ public class StoryDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story_detail);
-        mItem = getIntent().getParcelableExtra(EXTRA_STORY);
-        mStoryDetailFragment = StoryDetailFragment.newInstance(mItem);
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.container, mStoryDetailFragment, STORY_FRAG_TAG)
-                .commit();
+        mStoryItem = getIntent().getParcelableExtra(EXTRA_STORY);
+        if (shouldShowStoryDetailFragment(savedInstanceState)) {
+            showStoryDetailFragment(mStoryItem);
+        } else {
+            showCommentsFragment(mStoryItem);
+        }
+    }
+
+    private boolean shouldShowStoryDetailFragment(Bundle savedInstanceState) {
+        return savedInstanceState == null ||
+                savedInstanceState.getBoolean(SAVE_STATE_DETAIL_FRAG_SHOW);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        boolean storyDetailFragShowing = getSupportFragmentManager().findFragmentByTag(STORY_FRAG_TAG) != null;
+        outState.putBoolean(SAVE_STATE_DETAIL_FRAG_SHOW, storyDetailFragShowing);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_view_comments:
-                getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.container, getCommentsFragment(mItem), COMMENT_FRAG_TAG)
-                        .commit();
+                showCommentsFragment(mStoryItem);
                 break;
             case R.id.action_view_story:
-                getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.container, mStoryDetailFragment, STORY_FRAG_TAG)
-                        .commit();
+                showStoryDetailFragment(mStoryItem);
                 break;
         }
         return super.onOptionsItemSelected(item);
     }
 
-    public Fragment getCommentsFragment(Item item) {
+    //----------------------------------------------------------------------------------
+    // Helpers
+    //----------------------------------------------------------------------------------
+    private void showStoryDetailFragment(Item item) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, getStoryDetailFragment(item), STORY_FRAG_TAG)
+                .commit();
+    }
+
+    private void showCommentsFragment(Item item) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, getCommentsFragment(item), COMMENT_FRAG_TAG)
+                .commit();
+    }
+
+    private Fragment getCommentsFragment(Item item) {
         if (mCommentsFragment == null) {
             mCommentsFragment = CommentsFragment.newInstance(item);
         }
         return mCommentsFragment;
+    }
+
+    private Fragment getStoryDetailFragment(Item item) {
+        if (mStoryDetailFragment == null) {
+            mStoryDetailFragment = StoryDetailFragment.newInstance(mStoryItem);
+        }
+        return mStoryDetailFragment;
     }
 }
