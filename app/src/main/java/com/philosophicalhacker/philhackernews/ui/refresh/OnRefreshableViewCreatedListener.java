cfa907a09177f527e03148e633633a62de543b17
MattDupree <kmdupr33@gmail.com>
2015-07-22 06:21:35 -0400

    fixed warning in OnRefreshableViewCreatedListener

--- app/src/main/java/com/philosophicalhacker/philhackernews/ui/refresh/OnRefreshableViewCreatedListener.java
+++ app/src/main/java/com/philosophicalhacker/philhackernews/ui/refresh/OnRefreshableViewCreatedListener.java
@@ -3,6 +3,7 @@ package com.philosophicalhacker.philhackernews.ui.refresh;
 import android.view.View;
 
 /**
+ *
  * Created by MattDupree on 7/21/15.
  */
 public interface OnRefreshableViewCreatedListener {
